e9af7149234545e723f3edabbbd90fe59def9cff
Tibi <tothtibi30@gmail.com>
2021-03-16 22:39:43 +0100

    Implemented switch language component

---
+++ src/SwitchLanguage.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+const SwitchLanguage = () => {
+  const { i18n } = useTranslation();
+
+  const actualLanguage = i18n.language;
+
+  const handleChange = (e) => {
+    i18n.changeLanguage(e.target.value);
+    console.log(actualLanguage);
+  };
+
+  return (
+    <select
+      className="form-select m-4"
+      value={actualLanguage}
+      onChange={handleChange}
+    >
+      <option value="en">🇬🇧 English</option>
+      <option value="de">🇩🇪 Deutsch</option>
+      <option value="hu">🇭🇺 Magyar</option>
+    </select>
+  );
+};
+
+export default SwitchLanguage;
