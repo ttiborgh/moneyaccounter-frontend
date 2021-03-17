9ccf385428e41dff254f72da5d45569076548340
Tibi <tothtibi30@gmail.com>
2021-03-17 21:49:35 +0100

    Added bootstrap icon to Header, which triggers a dropdown login form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,23 @@
 import React from "react";
 import SwitchLanguage from "./SwitchLanguage";
-import LoginForm from "./LoginForm"
+import LoginForm from "./LoginForm";
+import { PeopleFill } from "react-bootstrap-icons";
+import { Dropdown } from "react-bootstrap";
 
-const Header = ({setLoggedIn, setUserId}) => {
+const Header = ({ setLoggedIn, setUserId }) => {
   return (
-    <div className="container-fluid m-0 header">
+    <div className="container-fluid header">
       <div className="row">
-        <div className="col-10">
-          <p>Sparschwein</p>
-        </div>
-        <div>
-          <LoginForm setLoggedIn={setLoggedIn} setUserId={setUserId} />
+        <div className="col-9"></div>
+        <div className="col-1 my-3 d-flex">
+          <Dropdown drop="down">
+            <Dropdown.Toggle variant="none">
+              <PeopleFill style={{ fontSize: "2rem" }} />
+            </Dropdown.Toggle>
+            <Dropdown.Menu style={{ backgroundColor: "antiquewhite" }}>
+              <LoginForm setLoggedIn={setLoggedIn} setUserId={setUserId} />
+            </Dropdown.Menu>
+          </Dropdown>
         </div>
         <div className="col-2 d-flex">
           <SwitchLanguage />
