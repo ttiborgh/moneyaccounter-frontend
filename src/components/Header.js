7af8cc7e453184f17170294e40d9840f3710ed2c
Tibi <tothtibi30@gmail.com>
2021-03-17 20:14:41 +0100

    Placed LoginForm into Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 import React from "react";
 import SwitchLanguage from "./SwitchLanguage";
+import LoginForm from "./LoginForm"
 
-const Header = () => {
+const Header = ({setLoggedIn, setUserId}) => {
   return (
     <div className="container-fluid m-0 header">
       <div className="row">
         <div className="col-10">
           <p>Sparschwein</p>
+        </div>
+        <div>
+          <LoginForm setLoggedIn={setLoggedIn} setUserId={setUserId} />
         </div>
         <div className="col-2 d-flex">
           <SwitchLanguage />
